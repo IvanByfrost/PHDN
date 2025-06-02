0f6f4d84ccdcdc6e596ea44bd853c2f3fff743d3
IvanByFrost <ivanwander99@gmail.com>
2025-06-02 09:32:54 -0500

    Refactor index.php to improve variable naming and enhance URL parameter validation

--- SalesSystem/index.php
+++ SalesSystem/index.php
@@ -8,19 +8,19 @@ $method = "";
 $params = "";
 
 $url = $_GET['url'] ?? 'Index/index';
-$array = explode('/', $url);
+$arrayUrl = explode('/', $url);
 if (isset($arrayUrl[0])){
-    $controller = $array[0];
+    $controller = $arrayUrl[0];
 }
 
 if (isset($arrayUrl[1])){
-    if ($arrayUrl[1] !=" ") {
+    if ($arrayUrl[1] !="") {
         $method = $arrayUrl[1];
     }
 }
 
 if (isset($arrayUrl[2])){
-    if ($arrayUrl[2] !=" ") {
+    if ($arrayUrl[2] !="") {
         $params = $arrayUrl[2];
     }
 }
