de6f43a0c975e8d20b4566fe14f90f6bdc4fa56c
IvanByFrost <ivanwander99@gmail.com>
2025-06-02 09:15:56 -0500

    Fix URL handling in index.php by updating variable names and ensuring proper extraction of controller, method, and parameters

--- SalesSystem/index.php
+++ SalesSystem/index.php
@@ -6,4 +6,25 @@ require 'config.php';
 $controller = "";
 $method = "";
 $params = "";
+
+$url = $_GET['url'] ?? 'Index/index';
+$array = explode('/', $url);
+if (isset($arrayUrl[0])){
+    $controller = $array[0];
+}
+
+if (isset($arrayUrl[1])){
+    if ($arrayUrl[1] !=" ") {
+        $method = $arrayUrl[1];
+    }
+}
+
+if (isset($arrayUrl[2])){
+    if ($arrayUrl[2] !=" ") {
+        $params = $arrayUrl[2];
+    }
+}
+
+echo $controller." ".$method." ".$params;
+
 ?>
