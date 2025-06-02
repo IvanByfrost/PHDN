f8d1f6c931ffde21e95a376fd31223b5cdeb7c75
IvanByFrost <ivanwander99@gmail.com>
2025-06-02 09:09:38 -0500

    Refactor index.php to comment out URL handling and add config.php with constants and definitions

---
+++ SalesSystem/config.php
@@ -0,0 +1,6 @@
+<?php
+const LBS = 'Library/';
+const VIEWS = 'Views/';
+define ('DFT', 'Default/');
+define ('RQ', 'Resources/');
+define ('URL', 'http://localhost/phdn/SalesSystem/');
--- SalesSystem/index.php
+++ SalesSystem/index.php
@@ -1,4 +1,9 @@
 <?php
-$url = $_GET['url'] ?? 'Index/index';
-echo $url;
+/* $url = $_GET['url'] ?? 'Index/index';
+echo $url; */
+
+require 'config.php';
+$controller = "";
+$method = "";
+$params = "";
 ?>
