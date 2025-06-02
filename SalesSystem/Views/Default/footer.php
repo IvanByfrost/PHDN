b9cc5b226533f682a1e85eabe92c7ae64ddb153e
IvanByFrost <ivanwander99@gmail.com>
2025-06-02 10:36:36 -0500

    Add header and footer templates for the Sales System views

---
+++ SalesSystem/Views/Default/footer.php
@@ -0,0 +1,10 @@
+<footer class="border-top footer text-muted">
+    <div align="center">
+        <p>&copy;PDHN</p>
+    </div>
+    <script src="<?php echo URL . RQ ?>js/jquery-3.6.0.min.js"></script>
+    <script src="<?php echo URL . RQ ?>js/bootstrap.min.js"></script>
+</footer>
+</body>
+
+</html>
